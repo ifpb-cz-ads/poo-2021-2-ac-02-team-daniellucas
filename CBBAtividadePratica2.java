0392a9e3e120e80bebff5cd917e4b795f7ba6073
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2022-05-11 21:41:48 -0300

    Questao 2 - CBB

---
+++ CBBAtividadePratica2.java
@@ -0,0 +1,14 @@
+public class CBBAtividadePratica2 {  
+    public static void main(String args[]) {  
+      int L=9;  
+      int D=10;  
+      int H=8;  
+       
+      int soma=L+D+H;  
+      int media=soma/3;  
+       
+ 
+      System.out.println("A soma das 3 variaveis é:  " + soma);  
+      System.out.println("A média é: " + media);  
+    }  
+}
