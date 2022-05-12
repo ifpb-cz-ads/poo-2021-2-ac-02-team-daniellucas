017674f3e9b7b8755b96a421bd007e31ab3889c5
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2022-05-11 21:43:48 -0300

    Questao 3 - CBB

---
+++ CBBAtividadePratica3.java
@@ -0,0 +1,10 @@
+public class CBBAtividadePratica3 {
+    public static void main(String args[]) {
+      int base=9;
+      int altura=10;
+      
+      int area=base*altura;
+
+      System.out.println("A area do retângulo é " + area);
+    }
+}
